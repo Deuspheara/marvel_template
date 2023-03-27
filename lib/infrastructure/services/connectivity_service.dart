ee113b985ed50eda1632620a346ed4b280d59852
Quentin Gaillardet <quentin.gaillardet01@gmail.com>
2023-03-27 10:15:34 +0200

    Add connectivity service

--- lib/infrastructure/services/connectivity_service.dart
+++ lib/infrastructure/services/connectivity_service.dart
@@ -1,6 +1,20 @@
 import 'package:injectable/injectable.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 
 @singleton
 class ConnectivityServive {
-  /// Implement the service
+  final Connectivity _connectivity;
+
+  ConnectivityServive(this._connectivity);
+
+  Future<bool> isConnected() async {
+    final ConnectivityResult result = await _connectivity.checkConnectivity();
+    return result != ConnectivityResult.none;
+  }
+
+  Stream<ConnectivityResult> get onConnectivityChanged =>
+      _connectivity.onConnectivityChanged;
+
+  Future<ConnectivityResult> get connectivityResult async =>
+      _connectivity.checkConnectivity();
 }
